f2f72092ebbd443a363fbc64ebbc1477c3479a41
FranzCantPickAName <soheil.rasouly@gmail.com>
2025-09-08 12:49:49 +0330

    Dispose final commit

--- Advanced Concepts 3/Program.cs
+++ Advanced Concepts 3/Program.cs
@@ -5,16 +5,18 @@ namespace Advanced_Concepts_3
 {
     internal class Program
     {
+        static void DoWork()
+        {
+            using Sample s = new Sample();
+            s.DisplayDataFromDatabase();
+        }
         static void Main()
         {
-            using (Sample s = new Sample())
-            {
-                s.DisplayDataFromDatabase();
-            }
+            DoWork();
 
             Console.WriteLine("Insert other stuff here");
 
-            //Console.ReadKey();
+            Console.ReadKey();
         }
     }
 }
