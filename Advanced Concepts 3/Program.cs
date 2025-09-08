041a26b3555293ce310a8adde16a6d55bd370503
FranzCantPickAName <soheil.rasouly@gmail.com>
2025-09-08 11:11:26 +0330

    Add project files.

---
+++ Advanced Concepts 3/Program.cs
@@ -0,0 +1,23 @@
+﻿using ClassLibrary1;
+using System;
+
+namespace Advanced_Concepts_3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            MarksCalculation.CalculationHelper ch = new MarksCalculation.CalculationHelper();
+
+            Console.WriteLine(ch.Multiply(10, 5));
+
+            MarksCalculation mc = new MarksCalculation();
+            Student s = new Student() { SecuredMarks = 35, MaxMarks = 50 };
+
+            mc.CalculatePercentage(s);
+            Console.WriteLine(s.Percentage);
+
+            Console.ReadKey();
+        }
+    }
+}
