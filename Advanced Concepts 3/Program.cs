8ad16a51d0ce354175ba245c6a36f4a1fbd0e46f
FranzCantPickAName <soheil.rasouly@gmail.com>
2025-09-13 12:03:11 +0330

    For & Foreach

--- Advanced Concepts 3/Program.cs
+++ Advanced Concepts 3/Program.cs
@@ -10,25 +10,19 @@ namespace Advanced_Concepts_3
             int[] a = new int[5] { 10, 20, 30, 40, 50 };
             string[] b = new string[5] { "One", "Two", "Three", "Four", "Five" };
 
-            //Console.WriteLine(a[0]);
-            //Console.WriteLine(a[1]);
-            //Console.WriteLine(a[2]);
-            //Console.WriteLine(a[3]);
-            //Console.WriteLine(a[4]);
-            //Console.WriteLine();
-            //Console.WriteLine(b[0]);
-            //Console.WriteLine(b[1]);
-            //Console.WriteLine(b[2]);
-            //Console.WriteLine(b[3]);
-            //Console.WriteLine(b[4]);
-
-            //Using for loop:
-            for (int i = 0; i < a.Length; i++)
+            foreach (int i in a)
             {
-                Console.WriteLine(a[i]);
+                Console.WriteLine(i);
             }
+
+            foreach (string i in b)
+            {
+                Console.WriteLine(i);
+            }
+
             Console.WriteLine();
-            for (int i = 0; i < b.Length; i++)
+
+            for (int i = b.Length - 1; i >= 0; i--)
             {
                 Console.WriteLine(b[i]);
             }
