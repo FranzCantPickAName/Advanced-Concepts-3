a5374bee39c6a262cfe58fb4ee71571e138c988e
FranzCantPickAName <soheil.rasouly@gmail.com>
2025-09-13 11:51:58 +0330

    Arrays first commit

--- Advanced Concepts 3/Program.cs
+++ Advanced Concepts 3/Program.cs
@@ -7,17 +7,34 @@ namespace Advanced_Concepts_3
     {
         static void Main()
         {
-            MarksCalculation.CalculationHelper ch = new MarksCalculation.CalculationHelper();
+            int[] a = new int[5] { 10, 20, 30, 40, 50 };
+            string[] b = new string[5] { "One", "Two", "Three", "Four", "Five" };
 
-            Console.WriteLine(ch.Multiply(10, 5));
+            //Console.WriteLine(a[0]);
+            //Console.WriteLine(a[1]);
+            //Console.WriteLine(a[2]);
+            //Console.WriteLine(a[3]);
+            //Console.WriteLine(a[4]);
+            //Console.WriteLine();
+            //Console.WriteLine(b[0]);
+            //Console.WriteLine(b[1]);
+            //Console.WriteLine(b[2]);
+            //Console.WriteLine(b[3]);
+            //Console.WriteLine(b[4]);
 
-            MarksCalculation mc = new MarksCalculation();
-            Student s = new Student() { SecuredMarks = 35, MaxMarks = 50 };
-
-            mc.CalculatePercentage(s);
-            Console.WriteLine(s.Percentage);
+            //Using for loop:
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+            Console.WriteLine();
+            for (int i = 0; i < b.Length; i++)
+            {
+                Console.WriteLine(b[i]);
+            }
 
             Console.ReadKey();
+
         }
     }
 }
