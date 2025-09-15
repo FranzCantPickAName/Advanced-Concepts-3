a47617fc1e3f11266954ae15f6defcbac272d426
FranzCantPickAName <soheil.rasouly@gmail.com>
2025-09-15 11:50:34 +0330

    List - Remove & Clear

--- Advanced Concepts 3/Program.cs
+++ Advanced Concepts 3/Program.cs
@@ -8,13 +8,17 @@ namespace Advanced_Concepts_3
     {
         static void Main()
         {
-            List<int> myList = new List<int>(10) { 10, 20, 30 };
+            List<int> myList = new List<int>(10) { 10, 20, 30, 40, 50 };
 
-            myList.Insert(1, 100);
+            //myList.Remove(30);
+            //myList.RemoveAt(2);
+            //myList.RemoveRange(1,2);
+            //myList.RemoveAll(n => n >= 30);
+            myList.Clear();
 
-            List<int> otherList = new List<int>() { 50, 60, 70 };
+            //List<int> otherList = new List<int>() { 50, 60, 70 };
 
-            myList.InsertRange(2, otherList);
+            //myList.InsertRange(2, otherList);
 
             Console.WriteLine("Using foreach loop: ");
             foreach (int item in myList)
