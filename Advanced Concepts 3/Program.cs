f36f9e44feadf1b5f60b32aac2e436f49df122e2
FranzCantPickAName <soheil.rasouly@gmail.com>
2025-09-15 12:48:24 +0330

    List - Exists & Find

--- Advanced Concepts 3/Program.cs
+++ Advanced Concepts 3/Program.cs
@@ -1,6 +1,7 @@
 ﻿using ClassLibrary1;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Advanced_Concepts_3
 {
@@ -9,17 +10,37 @@ namespace Advanced_Concepts_3
         static void Main()
         {
 
-            List<string> myFriends = new List<string>() { "Scott", "Allen", "James", "Jones" };
+            List<int> marks = new List<int>() { 40, 95, 24, 70, 16, 81 };
 
-            string[] myFriendsArray = myFriends.ToArray();
+            bool b = marks.Exists(m => m < 35);
 
-            foreach (string friend in myFriendsArray)
+            if (b)
             {
-                Console.WriteLine(friend);
+                Console.WriteLine("Student has failed one or more subjects.");
+            }
+            else
+            {
+                Console.WriteLine("Student has passed all subjects.");
             }
 
-            Console.WriteLine("\nForEach method: ");
-            myFriends.ForEach(friend => Console.WriteLine(friend));
+            int firstFailedMark = marks.Find(m => m < 35);
+            Console.WriteLine("The first failed mark is: " + firstFailedMark);
+
+            int firstFailedMarkIndex = marks.FindIndex(m => m < 35);
+            Console.WriteLine("The first failed mark is at index: " + firstFailedMarkIndex);
+
+            int lastFailedMark = marks.FindLast(m => m < 35);
+            Console.WriteLine("The last failed mark is: " + lastFailedMark);
+
+            int lastFailedMarkIndex = marks.FindLastIndex(m => m < 35);
+            Console.WriteLine("The last failed mark is at index: " + lastFailedMarkIndex);
+
+            List<int> allFailedMarks = marks.FindAll(m => m < 35);
+            Console.WriteLine("\nFailed marks: ");
+            foreach (int mark in allFailedMarks)
+            {
+                Console.WriteLine(mark);
+            }
 
             Console.ReadKey();
         }
