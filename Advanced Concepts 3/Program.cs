1013ae984237d40ece8dd8bd54fa3c4049b16374
FranzCantPickAName <soheil.rasouly@gmail.com>
2025-09-15 12:27:29 +0330

    List - Sort & Reverse

--- Advanced Concepts 3/Program.cs
+++ Advanced Concepts 3/Program.cs
@@ -8,32 +8,23 @@ namespace Advanced_Concepts_3
     {
         static void Main()
         {
-            Console.WriteLine("First list: ");
-            List<int> myList = new List<int>(10) { 10, 20, 30, 40, 50, 40 };
+            //Console.WriteLine("First list: ");
+            List<double> myNumbers = new List<double>() { 170, 150, 445, 120, 10, 4, 661 };
 
-            int n = myList.IndexOf(40);
-            Console.WriteLine("40 found at index " + n);
+            myNumbers.Sort();
 
-            int n2 = myList.IndexOf(60);
-            if (n2 >= 0)
+            foreach (double number in myNumbers)
             {
-                Console.WriteLine("60 found at index " + n2);
+                Console.WriteLine(number);
             }
-            else
+            Console.WriteLine();
+
+            myNumbers.Reverse();
+
+            foreach (double value in myNumbers)
             {
-                Console.WriteLine("60 not found");
+                Console.WriteLine(value);
             }
-
-            int n3 = myList.IndexOf(40, n + 1);
-            Console.WriteLine("Second occurence of 40 is at index " + n3);
-
-            Console.WriteLine("\nSecond list: ");
-            List<int> myList2 = new List<int>(10) { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
-            int n4 = myList2.BinarySearch(80);
-            Console.WriteLine("Binary search: 80 found at index " + n4);
-
-            bool b = myList2.Contains(110);
-            Console.WriteLine("110 is found: " + b);
 
             Console.ReadKey();
         }
