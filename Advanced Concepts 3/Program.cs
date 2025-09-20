d194e1ace48c22ab154daeb2c1062ae7ea16e882
FranzCantPickAName <soheil.rasouly@gmail.com>
2025-09-20 13:05:19 +0330

    Object Relations 1-1

--- Advanced Concepts 3/Program.cs
+++ Advanced Concepts 3/Program.cs
@@ -11,36 +11,20 @@ namespace Advanced_Concepts_3
     {
         static void Main()
         {
-            List<Product> products = new List<Product>();
+            Student student = new Student();
+            student.RollNo = 123;
+            student.StudentName = "Scott";
+            student.Email = "scott@gmail.com";
 
-            string choice;
-            do
-            {
-                Console.Write("Enter Product ID: ");
-                int pID = int.Parse(Console.ReadLine());
-                Console.Write("Enter Product Name: ");
-                string pName = Console.ReadLine();
-                Console.Write("Enter Product Price: ");
-                double unitPrice = double.Parse(Console.ReadLine());
-                Console.Write("Enter Date of Manufacture (YYYY-MM-DD) : ");
-                DateTime dateOfManufacture = DateTime.Parse(Console.ReadLine());
+            student.branch = new Branch();
+            student.branch.BranchName = "Computer Science Engineering";
+            student.branch.NoOfSemesters = 8;
 
-                Product product = new Product()
-                { ProductID = pID, ProductName = pName, ProductPrice = unitPrice, DateOfManufacture = dateOfManufacture };
-
-                products.Add(product);
-
-                Console.WriteLine("Product Added.\n");
-                Console.WriteLine("Do you want to continue to next product? (Yes/No)");
-                choice = Console.ReadLine();
-            }
-            while (choice != "No" && choice != "no" && choice != "n" && choice != "N");
-
-            Console.WriteLine("\nProducts:");
-            foreach (Product item in products)
-            {
-                Console.WriteLine(item.ProductID + ", " + item.ProductName + ", " + item.ProductPrice + ", " + item.DateOfManufacture.ToShortDateString());
-            }
+            Console.WriteLine(student.RollNo);
+            Console.WriteLine(student.StudentName);
+            Console.WriteLine(student.Email);
+            Console.WriteLine(student.branch.BranchName);
+            Console.WriteLine(student.branch.NoOfSemesters);
 
             Console.ReadKey();
         }
--- ClassLibrary2/Class1.cs
+++ ClassLibrary2/Class1.cs
@@ -6,14 +6,19 @@ namespace ClassLibrary1
     /// <summary>
     /// Represents a product in the ECommerce application
     /// </summary>
-    public class Product
+    public class Student
     {
-        public int ProductID { get; set; }
-        public string ProductName { get; set; }
-        public double ProductPrice { get; set; }
-        public DateTime DateOfManufacture {  get; set; }
+        public int RollNo { get; set; }
+        public string StudentName { get; set; }
+        public string Email { get; set; }
+        public Branch branch {  get; set; }
 
+    }
 
+    public class Branch
+    {
+        public string BranchName { get; set; }
+        public int NoOfSemesters { get; set; }
     }
 
 }
