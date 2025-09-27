d6750e0e161b51d30ad8a1295505c590a558e9ec
FranzCantPickAName <soheil.rasouly@gmail.com>
2025-09-27 11:00:27 +0330

    Covariance

--- Advanced Concepts 3/Program.cs
+++ Advanced Concepts 3/Program.cs
@@ -11,25 +11,14 @@ namespace Advanced_Concepts_3
     {
         static void Main()
         {
-            //collection of objects
-            List<Employee> employees = new List<Employee>()
-            {
-                new Employee() { EmpID = 104, EmpName = "Mary", Job = "Designer"},
-                new Employee() { EmpID = 102, EmpName = "Alexa", Job = "Manager"},
-                new Employee() { EmpID = 101, EmpName = "Steven", Job = "Consultant"},
-                new Employee() { EmpID = 103, EmpName = "Jade", Job = "Manager"},
-                new Employee() { EmpID = 105, EmpName = "Sara", Job = null}
-            };
+            LivingThing livingThing = new Parrot();
+            Parrot parrot = new Parrot() { NumberOfLegs = 2 };
 
-            CustomComparer customComparer = new CustomComparer();
-            customComparer.sortBy = SortBy.Job;
-            employees.Sort(customComparer); //EmpName
-            //employees.Reverse();
+            IMover<LivingThing> mover = new Mover<Parrot>() { thing = parrot };
+            Console.WriteLine("Moving with " + mover.Move().NumberOfLegs + " legs");
 
-            foreach (Employee emp in employees)
-            {
-                Console.WriteLine(emp.EmpID + ", " + emp.EmpName + ", " + emp.Job);
-            }
+            Sample sample = new Sample();
+            sample.PrintValues(new List<string>() { "hello", "world" });
 
             Console.ReadKey();
         }
--- ClassLibrary2/Class1.cs
+++ ClassLibrary2/Class1.cs
@@ -6,69 +6,46 @@ namespace ClassLibrary1
 {
 
     //moodel class
-    public class Employee
+    public class LivingThing
     {
-        public int EmpID { get; set; }
-        public string EmpName { get; set; }
-        public string Job { get; set; }
+        public int NumberOfLegs { get; set; }
+
     }
 
-    public enum SortBy
+    public class Parrot: LivingThing
     {
-        EmpID, EmpName, Job
+
     }
 
-    public class CustomComparer : IComparer<Employee>
+    public class Dog: LivingThing
     {
-        //Sort by EmpID
-        //public int Compare(Employee x, Employee y)
-        //{
-        //    return x.EmpID - y.EmpID;
-        //}
 
-        //Sort by EmpName
-        //public int Compare(Employee x, Employee y)
-        //{
-        //    return x.EmpName.CompareTo(y.EmpName);
-        //}
+    }
 
-        //Sort By Job, EmpName
-        //public int Compare(Employee x, Employee y)
-        //{
-        //    int result = 0;
-        //    if (x.Job != null)
-        //    {
-        //        result = x.Job.CompareTo(y.Job); //first sorting column
-        //    }
-        //    if (result == 0)
-        //    {
-        //        if (x.EmpName != null)
-        //        {
-        //            result = x.EmpName.CompareTo(y.EmpName); //second sorting column
-        //        }
-        //    }
-        //    return result;
-        //}
+    public interface IMover<out T>
+    {
+        T Move();
+    }
 
-        //Sort by selected column
-        public int Compare(Employee x, Employee y)
+    public class Mover<T> : IMover<T>
+    {
+        public T thing { get; set; }
+        public T Move()
         {
-            int result = 0;
-            switch (this.sortBy)
-            {
-                case SortBy.EmpID:
-                    result = x.EmpID - y.EmpID; break;
-                case SortBy.EmpName:
-                    result = (x.EmpName != null) ? x.EmpName.CompareTo(y.EmpName) : 0; break;
-                case SortBy.Job:
-                    result = (x.Job != null) ? x.Job.CompareTo(y.Job) : 0; break;
-                default:
-                    result = 0; break;
-            }
-            return result;
+            return thing;
         }
+    }
 
-        public SortBy sortBy { get; set; }
+    public class Sample
+    {
+        public void PrintValues(IEnumerable<object> values)
+        {
+            foreach (var item in values)
+            {
+                Console.WriteLine(item + ", ");
+            }
+            Console.WriteLine();
+        }
     }
 
 }
