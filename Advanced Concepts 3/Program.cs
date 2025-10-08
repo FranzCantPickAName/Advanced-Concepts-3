8c22fa369159b478b8daf06895a4de394a0b3c54
FranzCantPickAName <soheil.rasouly@gmail.com>
2025-10-08 12:12:58 +0330

    Encoding - ASCII

--- Advanced Concepts 3/Program.cs
+++ Advanced Concepts 3/Program.cs
@@ -7,49 +7,37 @@ namespace Advanced_Concepts_3
     {
         static void Main()
         {
+            Console.WriteLine("Character 'A': ");
 
-            Console.WriteLine("Binary: ");
+            char ch = 'A';
+            byte b = (byte)ch;
+            Console.WriteLine(b);
+            char ch2 = (char)b;
+            Console.WriteLine(ch2);
 
-            int dec1 = 13;
+            Console.WriteLine("\nBytes from 0 to 127: ");
 
-            //Display the decimal as a string in base 2
-            //Second argument is the base we WANT
-            string binary1 = Convert.ToString(dec1, 2);
-            Console.WriteLine(binary1);
+            byte[] bytes = new byte[128];
+            for (byte i = 0; i < 128; i++)
+            {
+                bytes[i] = i;
+            }
+            string s = System.Text.Encoding.ASCII.GetString(bytes);
+            Console.OutputEncoding = System.Text.Encoding.ASCII;
+            Console.WriteLine(s);
 
-            //Convert that binary string back to decimal
-            //Second argument is the base that we are INPUTTING
-            int dec2 = Convert.ToInt32(binary1, 2);
-            Console.WriteLine(dec2);
+            Console.WriteLine("\nSentence: ");
 
-            int n = 0b1100100;
-            n += 1;
-            Console.WriteLine(n);
+            string sentence = "The quick brown fox jumps over the lazy dog.";
+            byte[] bytes2 = System.Text.Encoding.ASCII.GetBytes(sentence);
+            foreach (byte item in bytes2)
+            {
+                Console.Write(item + " ");
+            }
 
-            Console.WriteLine("\nOctal: ");
-
-            int dec3 = 289;
-            string oct1 = Convert.ToString(dec3, 8);
-            Console.WriteLine(oct1);
-
-            int dec4 = Convert.ToInt32(oct1, 8);
-            Console.WriteLine(dec4);
-
-            //int n2 = 0144;
-            //Console.WriteLine(n2);
-
-            Console.WriteLine("\nHexadecimal: ");
-
-            int dec5 = 742;
-            string hex1 = Convert.ToString(dec5, 16);
-            Console.WriteLine(hex1);
-
-            int dec6 = Convert.ToInt32(hex1, 16);
-            Console.WriteLine(dec6);
-
-            int n3 = 0x64;
-            n3 += 1;
-            Console.WriteLine(n3);
+            Console.WriteLine();
+            string sentence2 = System.Text.Encoding.ASCII.GetString(bytes2);
+            Console.WriteLine(sentence2);
 
             Console.ReadKey();
         }
