4d5c5e429b7d9707bdac31923ec53330ca5e6ae4
FranzCantPickAName <soheil.rasouly@gmail.com>
2025-10-08 11:59:51 +0330

    Numbering system

--- Advanced Concepts 3/Program.cs
+++ Advanced Concepts 3/Program.cs
@@ -7,15 +7,49 @@ namespace Advanced_Concepts_3
     {
         static void Main()
         {
-            MarksCalculation.CalculationHelper ch = new MarksCalculation.CalculationHelper();
 
-            Console.WriteLine(ch.Multiply(10, 5));
+            Console.WriteLine("Binary: ");
 
-            MarksCalculation mc = new MarksCalculation();
-            Student s = new Student() { SecuredMarks = 35, MaxMarks = 50 };
+            int dec1 = 13;
 
-            mc.CalculatePercentage(s);
-            Console.WriteLine(s.Percentage);
+            //Display the decimal as a string in base 2
+            //Second argument is the base we WANT
+            string binary1 = Convert.ToString(dec1, 2);
+            Console.WriteLine(binary1);
+
+            //Convert that binary string back to decimal
+            //Second argument is the base that we are INPUTTING
+            int dec2 = Convert.ToInt32(binary1, 2);
+            Console.WriteLine(dec2);
+
+            int n = 0b1100100;
+            n += 1;
+            Console.WriteLine(n);
+
+            Console.WriteLine("\nOctal: ");
+
+            int dec3 = 289;
+            string oct1 = Convert.ToString(dec3, 8);
+            Console.WriteLine(oct1);
+
+            int dec4 = Convert.ToInt32(oct1, 8);
+            Console.WriteLine(dec4);
+
+            //int n2 = 0144;
+            //Console.WriteLine(n2);
+
+            Console.WriteLine("\nHexadecimal: ");
+
+            int dec5 = 742;
+            string hex1 = Convert.ToString(dec5, 16);
+            Console.WriteLine(hex1);
+
+            int dec6 = Convert.ToInt32(hex1, 16);
+            Console.WriteLine(dec6);
+
+            int n3 = 0x64;
+            n3 += 1;
+            Console.WriteLine(n3);
 
             Console.ReadKey();
         }
