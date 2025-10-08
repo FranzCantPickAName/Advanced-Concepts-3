e6a374a8d9f46a6e63d55b188d094268e137759a
FranzCantPickAName <soheil.rasouly@gmail.com>
2025-10-08 11:30:12 +0330

    LINQ - Many things

--- Advanced Concepts 3/Program.cs
+++ Advanced Concepts 3/Program.cs
@@ -13,11 +13,11 @@ namespace Advanced_Concepts_3
         {
             List<Employee> employees = new List<Employee>()
             {
-                new Employee() { EmpID = 101, EmpName = "Henry", Job = "Designer", City = "Boston" },
-                new Employee() { EmpID = 102, EmpName = "Jack", Job = "Developer", City = "New York" },
-                new Employee() { EmpID = 103, EmpName = "Gabriel", Job = "Analyst", City = "Tokyo" },
-                new Employee() { EmpID = 104, EmpName = "William", Job = "Manager", City = "Tokyo" },
-                new Employee() { EmpID = 105, EmpName = "Alexa", Job = "Manager", City = "New York" }
+                new Employee() { EmpID = 101, EmpName = "Henry", Job = "Designer", City = "Boston", Salary = 100000 },
+                new Employee() { EmpID = 102, EmpName = "Jack", Job = "Developer", City = "New York", Salary = 150000 },
+                new Employee() { EmpID = 103, EmpName = "Gabriel", Job = "Analyst", City = "Tokyo", Salary = 300000 },
+                new Employee() { EmpID = 104, EmpName = "William", Job = "Manager", City = "Tokyo", Salary = 400000 },
+                new Employee() { EmpID = 105, EmpName = "Alexa", Job = "Manager", City = "New York", Salary = 200000 }
             };
 
 
@@ -62,6 +62,7 @@ namespace Advanced_Concepts_3
 
             Console.WriteLine("\nSearching from the last: \n");
 
+            //Using Where
             List<Employee> result2 = employees.Where(emp => emp.Job == "Manager").ToList();
             Console.WriteLine(result2[result2.Count - 1].EmpID + ", " + result2[result2.Count - 1].EmpName + ", " + result2[result2.Count - 1].Job + ", " + result2[result2.Count - 1].City);
 
@@ -78,6 +79,56 @@ namespace Advanced_Concepts_3
             {
                 Console.WriteLine("No clerks have been found in the list.");
             }
+
+            //ElementAt
+            Console.WriteLine("\nSearching at element: \n");
+
+            Employee resultEmp = employees.Where(emp => emp.Job == "Manager").ElementAt(1);
+            Console.WriteLine(resultEmp.EmpID + ", " + resultEmp.EmpName + ", " + resultEmp.Job + ", " + resultEmp.City);
+
+            Employee resultEmp2 = employees.Where(emp => emp.Job == "Manager").ElementAtOrDefault(4);
+            if (resultEmp2 != null)
+            {
+                Console.WriteLine(resultEmp2.EmpID + ", " + resultEmp2.EmpName + ", " + resultEmp2.Job + ", " + resultEmp2.City);
+            }
+            else
+            {
+                Console.WriteLine("No managers found at index 4.");
+            }
+
+            //Single
+            //Either method will throw an exception if more than 1 matching element is found
+            Console.WriteLine("\nSearching for single element: \n");
+
+            Employee singleResult = employees.Single(emp => emp.EmpID == 102);
+            Console.WriteLine(singleResult.EmpID + ", " + singleResult.EmpName + ", " + singleResult.Job + ", " + singleResult.City);
+
+            Employee singleResult2 = employees.SingleOrDefault(emp => emp.Job == "Clerk");
+            if (singleResult2 != null)
+            {
+                Console.WriteLine(singleResult2.EmpID + ", " + singleResult2.EmpName + ", " + singleResult2.Job + ", " + singleResult2.City);
+            }
+            else
+            {
+                Console.WriteLine("No matching employee.");
+            }
+
+            //Select
+            Console.WriteLine("\nListing employees as Person(s): \n");
+
+            List<Person> people = employees.Select(emp => new Person() { PersonName = emp.EmpName }).ToList();
+            foreach (Person person in people)
+            {
+                Console.WriteLine(person.PersonName);
+            }
+
+            Console.WriteLine();
+            //Min, Max, Sum, Avg, Count
+            Console.WriteLine("The employee with the lowest salary earns: " + employees.Min(emp => emp.Salary));
+            Console.WriteLine("The employee with the highest salary earns: " + employees.Max(emp => emp.Salary));
+            Console.WriteLine("The sum of all employee salaries is: " + employees.Sum(emp => emp.Salary));
+            Console.WriteLine("The avergae salary is: " + employees.Average(emp => emp.Salary));
+            Console.WriteLine("The total number of employees is: " + employees.Count());
 
             Console.ReadKey();
         }
--- ClassLibrary2/Class1.cs
+++ ClassLibrary2/Class1.cs
@@ -2,12 +2,18 @@
 
 namespace ClassLibrary1
 {
+
+    public class Person
+    {
+        public string PersonName { get; set; }
+    }
     public class Employee
     {
         public int EmpID { get; set; }
         public string EmpName { get; set; }
         public string Job { get; set; }
         public string City { get; set; }
+        public double Salary { get; set; }
     }
 
 }
